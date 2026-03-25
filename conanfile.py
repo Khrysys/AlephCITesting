39d19346cead46064fdb0d93eb7529409530dc22
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2026-03-25 14:01:55 -0400

    Add Conan package configuration in conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,92 @@
+#type: ignore
+# Ignore typing for the whole file, since all it does is throw errors on my machine.
+# Easy enough to validate by hand.
+import json
+import os
+import re
+
+from conan import ConanFile
+from conan.api.output import ConanOutput
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
+from conan.tools.build import check_min_cppstd
+from conan.tools.sbom import cyclonedx_1_6
+from pathlib import Path
+
+
+def post_generate(conanfile, **kwargs):
+    sbom_cyclonedx_1_6 = cyclonedx_1_6(conanfile)
+    generators_folder = conanfile.generators_folder
+    file_name = "sbom.cdx.json"
+    folder = Path(generators_folder) / "sbom"
+    folder.mkdir(parents=True, exist_ok=True)
+    file = folder / file_name
+    with file.open('w') as f:
+        json.dump(sbom_cyclonedx_1_6, f, indent=4)
+        
+    ConanOutput().success(f"CYCLONEDX CREATED - {conanfile.generators_folder}")
+
+project_regex_string = r"""project\s*\(\s*([a-z]+).*VERSION\s+([^\s]+)\s*\)\s*\n"""
+
+class AlephConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def build_requirements(self):
+        self.tool_requires('cmake/4.2.1')
+        self.tool_requires("b2/5.x", override=True)
+
+        self.test_requires('gtest/1.17.0')
+        self.test_requires('benchmark/1.9.4')
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+        post_generate(self)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def set_name(self):
+        cmake = Path(self.recipe_folder) / "CMakeLists.txt"
+        content = cmake.read_text(encoding="utf-8")
+
+        m = re.search(project_regex_string, content, re.IGNORECASE | re.VERBOSE | re.DOTALL)
+
+        if not m:
+            raise RuntimeError("Could not extract project name from CMakeLists.txt")
+
+        self.name = m.group(1).lower()
+
+    def set_version(self):
+        cmake = Path(self.recipe_folder) / "CMakeLists.txt"
+        content = cmake.read_text(encoding="utf-8")
+
+        m = re.search(project_regex_string, content, re.IGNORECASE | re.VERBOSE | re.DOTALL)
+
+        if not m:
+            raise RuntimeError("Could not extract version from project() in CMakeLists.txt")
+
+        self.version = m.group(2)
+
+    def requirements(self):
+        self.requires('boost/1.90.0')
+        self.requires('fmt/12.1.0')
+        self.requires('libassert/2.2.1')
+        self.requires('spdlog/1.17.0')
+
+        # Force specific versions for transitive dependencies
+        self.requires('b2/5.4.2', override=True)
+        self.requires('cpptrace/1.0.4', override=True)
+        self.requires('libdwarf/2.1.0', override=True)
+        self.requires('zlib/1.3.1', override=True)
+        self.requires(' zstd/1.5.7', override=True)
+            
+    def validate(self):
+        check_min_cppstd(self, 20)
